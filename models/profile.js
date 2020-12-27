6539ce8bc022d6212b83fdacac0dedd046c21be2
Ryan Fanelli <illenafnayr@gmail.com>
2020-12-27 15:15:46 -0500

    schema updated

--- models/profile.js
+++ models/profile.js
@@ -2,9 +2,16 @@ const mongoose = require('mongoose')
 
 const profileSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    DOB: {type: Number, required: true},
-    Address: {type: String, required: true},
-    contact: {type: String, required: true},
+    dob: {type: Number, required: true},
+    address: {
+        addressLine1: {type: String},
+        addressLine2: {type: String},
+        addressLine3: {type: String},
+        city: {type: String},
+        region: {type: String},
+        postalCode: {type: String},
+        country: {type: String, required: true}
+    },
     covid19Data: {
         hadTest: {type: Boolean, required: true},
         testInfo: {
