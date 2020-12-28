4ca60cdee9e0cd4943045ef4036a51539daaf83c
Ryan Fanelli <illenafnayr@gmail.com>
2020-12-28 13:14:58 -0500

    can click on map and returns address vie reverse geocoding

--- public/app.js
+++ public/app.js
@@ -17,16 +17,28 @@
 
 $(()=>{
     const initMap = ()=>{
-        let map = new google.maps.Map(document.getElementById('map'), {
+        const map = new google.maps.Map(document.getElementById('map'), {
             center: { lat: 45, lng: -75 },
             zoom: 12
         })
+
+        const geocoder = new google.maps.Geocoder()
     
         //listen for click on map
         
         google.maps.event.addListener(map, 'click', (event)=>{
-            // console.log(event.latLng);
+            console.log(event.latLng);
             addMarker(event.latLng)
+            geocoder.geocode({
+                'latLng': event.latLng
+              },
+              (results, status)=>{
+                if (status == google.maps.GeocoderStatus.OK) {
+                  if (results[0]) {
+                    alert(results[0].formatted_address);
+                  }
+                }
+            })
         })
         
         // let marker = new google.maps.Marker({
@@ -85,6 +97,26 @@ $(()=>{
                 infoWindow.open(map, marker);
               });
         }
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+              (position) => {
+                const pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude,
+                };
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("Your Location");
+                infoWindow.open(map);
+                map.setCenter(pos);
+              },
+              () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+              }
+            );
+          } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+          }
     }
     
     google.maps.event.addDomListener(window, 'load', initMap);
